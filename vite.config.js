ebe21c7f42d180e929dbeaffeba4c64ce8ba4163
daoyu <daoyu1024@icloud.com>
2025-01-06 08:24:05 +0000

    test github pages

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    outDir: 'docs'
+  },
   server: {
     host: '127.0.0.1',
     port: 5500
